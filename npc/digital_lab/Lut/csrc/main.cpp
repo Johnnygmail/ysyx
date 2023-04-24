e3b3b59d2ca602549c99aa63ae3c9b8df7ac96e4
Johnny <jiangzhong.personal@gmail.com>
2023-04-24 16:00:51 +0800

    third commit

--- npc/digital_lab/Lut/csrc/main.cpp
+++ npc/digital_lab/Lut/csrc/main.cpp
@@ -5,7 +5,7 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(Vtop* top);
 
-  static void single_cycle() {
+/*  static void single_cycle() {
   dut.clk = 0; dut.eval();
   dut.clk = 1; dut.eval();
 }
@@ -14,17 +14,18 @@ static void reset(int n) {
   dut.rst = 1;
   while (n -- > 0) single_cycle();
   dut.rst = 0;
-}
+}*/
 
 int main() {
   nvboard_bind_all_pins(&dut);
   nvboard_init();
 
-  reset(10);
+//  reset(10);
 
   while(1) {
     nvboard_update();
-    single_cycle();
+    dut.eval();
+//    single_cycle();
   }
 }
 
---
+++ npc/digital_lab/decoder/csrc_sim/sim_main.cpp
@@ -0,0 +1,44 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vtop.h"
+#include<stdio.h>
+
+
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+
+static Vtop* top;
+
+void step_and_dump_wave(){
+  top->eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());
+}
+void sim_init(){
+  contextp = new VerilatedContext;
+  tfp = new VerilatedVcdC;
+  top = new Vtop;
+  contextp->traceEverOn(true);
+  top->trace(tfp, 0);
+  tfp->open("wave.vcd");
+}
+
+void sim_exit(){
+  step_and_dump_wave();
+  tfp->close();
+}
+
+int main() {
+  sim_init();
+
+   top->Y=0; top->X0=0;top->X1=0; top->X2=0; top->X3=0;  step_and_dump_wave();   
+                      top->X0=3;  step_and_dump_wave();   // 将b改为“1”，s和a的值不变，继续保持“0”，
+            top->Y=1; top->X1=1;  step_and_dump_wave();   // 将a，b分别改为“1”和“0”，s的值不变，
+                      top->X1=2;  step_and_dump_wave();   // 将b改为“1”，s和a的值不变，维持10个时间单位
+  top->Y=2; top->X2=1;  step_and_dump_wave();   // 将s，a，b分别变为“1,0,0”，维持10个时间单位
+                      top->Y=3;  step_and_dump_wave();
+                      top->Y=1;  step_and_dump_wave();
+                      top->Y=0;  step_and_dump_wave();
+
+  sim_exit();
+}
